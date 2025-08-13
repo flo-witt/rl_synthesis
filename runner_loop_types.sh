acca622a6a77b494c1e3da65fa5a6cd68dd08b75
David Hudák <61205638+DaveHudiny@users.noreply.github.com>
2025-08-13 08:35:01 +0200

    Update runner_loop_types.sh
    Adjusted the runner script.

--- runner_loop_types.sh
+++ runner_loop_types.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source prerequisites/venv/bin/activate
+trap 'echo "Caught Ctrl+C, killing all subprocesses..."; pkill -P $$; exit 1' SIGINT
 
 # This script runs the robust extraction on all models in the specified directory.
 
@@ -18,7 +18,7 @@ log_folder="experiments_logs_loop_types"
 mkdir -p "$log_folder"
 
 for i in $(seq 1 $seeds); do
-# Loop through all directories in the model directory
+    # Loop through all directories in the model directory
     for project_path in "$model_dir"/*/; do
         # Check if the directory is not empty
         model_name=$(basename "$project_path")
@@ -27,22 +27,36 @@ for i in $(seq 1 $seeds); do
             echo "Running extraction for model in $project_path"
             
             for extraction_with in "${extraction_with[@]}"; do 
-            # if it is without extraction, run only restarts and without restarts. Do not use geometric flags
-            if [ "$extraction_with" == "--without-extraction" ]; then
-                for restart in "${restarts[@]}"; do
-                    echo "Running without extraction for model in $project_path with restart: $restart, extraction_with: $extraction_with, and standard flags: $standard_flags"
-                    timeout 7200 python robust_pomdps_rl.py --project-path "$project_path" $standard_flags $restart $extraction_with > "$log_folder/${model_name}_without_extraction_restart_${restart}.log" 2> "$log_folder/${model_name}_without_extraction_restart_${restart}.err"
-                done
-            else
-                # Run with geometric flags and restarts
-                for geometric_flag in "${geometric_flags[@]}"; do
+                # if it is without extraction, run only restarts and without restarts. Do not use geometric flags
+                if [ "$extraction_with" == "--without-extraction" ]; then
                     for restart in "${restarts[@]}"; do
-                        echo "Running extraction for model in $project_path with geometric flag: $geometric_flag and restart: $restart, extraction_with: $extraction_with, and standard flags: $standard_flags"
-                        timeout 7200 python robust_pomdps_rl.py --project-path "$project_path" $standard_flags $geometric_flag $restart $extraction_with > "$log_folder/${model_name}_geometric_${geometric_flag}_restart_${restart}.log" 2> "$log_folder/${model_name}_geometric_${geometric_flag}_restart_${restart}.err"
+                        echo "Running without extraction for model in $project_path with restart: $restart, extraction_with: $extraction_with, and standard flags: $standard_flags"
+                        timeout 7200 prerequisites/venv/bin/python3.10 \
+                            robust_pomdps_rl.py \
+                            --project-path "$project_path" \
+                            $standard_flags \
+                            $restart \
+                            $extraction_with \
+                            > "$log_folder/${model_name}_without_extraction_restart_${restart}_${i}.log" \
+                            2> "$log_folder/${model_name}_without_extraction_restart_${restart}_${i}.err"
                     done
-                done
-            fi
-
+                else
+                    # Run with geometric flags and restarts
+                    for geometric_flag in "${geometric_flags[@]}"; do
+                        for restart in "${restarts[@]}"; do
+                            echo "Running extraction for model in $project_path with geometric flag: $geometric_flag and restart: $restart, extraction_with: $extraction_with, and standard flags: $standard_flags"
+                            timeout 7200 prerequisites/venv/bin/python3.10 \
+                                robust_pomdps_rl.py \
+                                --project-path "$project_path" \
+                                $standard_flags \
+                                $geometric_flag \
+                                $restart \
+                                $extraction_with \
+                                > "$log_folder/${model_name}_geometric_${geometric_flag}_restart_${restart}_${i}.log" \
+                                2> "$log_folder/${model_name}_geometric_${geometric_flag}_restart_${restart}_${i}.err"
+                        done
+                    done
+                fi
             done
         else
             echo "Skipping empty directory: $project_path"
@@ -51,5 +65,4 @@ for i in $(seq 1 $seeds); do
 done
 
 # Deactivate the virtual environment
-deactivate
 echo "Extraction completed for all models in $model_dir."
